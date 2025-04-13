c533b8ab3ba6acd796dddd1e80b10f916592fdf8
Aice <alicelourencoreis@gmail.com>
2025-04-13 12:35:55 -0300

    commit

--- src/utils/slug.ts
+++
@@ -1,4 +0,0 @@
-export const criarSlug = (nome: string) =>
-    nome.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "") // remove acentos
-        .replace(/\s+/g, "-") // espaços viram hífen
-        .replace(/[^a-z0-9-]/g, ""); // remove caracteres especiais
